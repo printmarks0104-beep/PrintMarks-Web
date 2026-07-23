98609cd24cad8380bc07e7fc4644c797c6383e35
printmarks0104-beep <printmarks0104@gmail.com>
2026-07-23 17:49:13 +0800

    Update config.template.js

--- config.template.js
+++ config.template.js
@@ -1,6 +1,5 @@
 window.PRINTMARKS_CONFIG = {
-  SUPABASE_URL: "https://printmarks0104-beep.github.io/PrintMarks-Web/**
-http://localhost:8080/**",
+  SUPABASE_URL: "https://lbxowimckjbmqfobjqlw.supabase.co",
   SUPABASE_ANON_KEY: "sb_publishable_8Cp6ouI0lmQjLPzxKdrZYA_gjko-YVX",
   DOWNLOAD_URL: "downloads/PrintMarks_Setup.exe",
   SUPPORT_EMAIL: "printmarks0104@gmail.com"
