e39f658c72e5d4095ac27c1ef56ba4acff30dc05
printmarks0104-beep <printmarks0104@gmail.com>
2026-07-23 14:03:36 +0800

    Add PRINTMARKS_CONFIG to config.js

---
+++ config.js
@@ -0,0 +1,7 @@
+window.PRINTMARKS_CONFIG = {
+  SUPABASE_URL: "https://printmarks0104-beep.github.io/PrintMarks-Web/**
+http://localhost:8080/**",
+  SUPABASE_ANON_KEY: "sb_publishable_8Cp6ouI0lmQjLPzxKdrZYA_gjko-YVX",
+  DOWNLOAD_URL: "downloads/PrintMarks_Setup.exe",
+  SUPPORT_EMAIL: "support@example.com"
+};
