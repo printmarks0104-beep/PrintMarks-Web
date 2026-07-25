003b09ab0ccf5f8bc75167cb98a048524a68765d
printmarks0104-beep <printmarks0104@gmail.com>
2026-07-25 23:39:29 +0800

    Update config.template.js

--- config.template.js
+++ config.template.js
@@ -1,7 +1,7 @@
 window.PRINTMARKS_CONFIG = {
   SUPABASE_URL: "https://lbxowimckjbmqfobjqlw.supabase.co",
   SUPABASE_ANON_KEY: "sb_publishable_8Cp6ouI0lmQjLPzxKdrZYA_gjko-YVX",
-  DOWNLOAD_URL: "downloads/PrintMarks_Setup.exe",
+  DOWNLOAD_URL: "https://github.com/printmarks0104-beep/PrintMarks-Downloads/releases/latest/download/PrintMarks_Setup.exe",
   GCASH_NUMBER: "09919925513",
   BDO_ACCOUNT_NUMBER: "5210691703361957",
   BDO_ACCOUNT_NAME: "",
