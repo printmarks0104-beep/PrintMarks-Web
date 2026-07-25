75b2e6ab3db330ec2abfd731091889d250573d66
printmarks0104-beep <printmarks0104@gmail.com>
2026-07-25 23:37:27 +0800

    Add payment configuration details to template

--- config.template.js
+++ config.template.js
@@ -2,5 +2,9 @@ window.PRINTMARKS_CONFIG = {
   SUPABASE_URL: "https://lbxowimckjbmqfobjqlw.supabase.co",
   SUPABASE_ANON_KEY: "sb_publishable_8Cp6ouI0lmQjLPzxKdrZYA_gjko-YVX",
   DOWNLOAD_URL: "downloads/PrintMarks_Setup.exe",
+  GCASH_NUMBER: "09919925513",
+  BDO_ACCOUNT_NUMBER: "5210691703361957",
+  BDO_ACCOUNT_NAME: "",
+  PAYMENT_SUPPORT_EMAIL: "printmarks0104@gmail.com",
   SUPPORT_EMAIL: "printmarks0104@gmail.com"
 };
